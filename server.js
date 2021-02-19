4b1d5278d17170d4d77476052ba7e867a419362f
Jerome Mico Jaropojop <jmjaropojop92@gmail.com>
2021-02-20 01:41:55 +0800

    [package.json]: Installed uuid package for unique ID generation; [utils folder]: folder for helper functions; [server.js]: imported the helper functions from utils.js; Created socket.io events;

--- server.js
+++ server.js
@@ -3,15 +3,72 @@ const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 const http = require('http');
-const socketio = require('socket.io');
+const socketio = require("socket.io");
 require('dotenv').config();
+// Utils
+const { 
+  playerJoin, 
+  createRoom, 
+  getCurrentRoom, 
+  removePlayer, 
+  getRoomPlayers 
+} = require('./utils/utils');
 
 // Set up Express
 const app = express();
 
+app.use(express.json({ extended: false }));
+
+// Setup socket.io server
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
 app.use(cors());
 
-app.use(express.json({ extended: false }));
+// Run when a client connects
+io.on('connection', socket => {
+  // Listen to 'newGame' event
+  socket.on("createGame", () => {
+    const room = createRoom();
+    // Emit 'roomCreated' event to client passing the room ID
+    socket.emit("roomCreated", room);
+  });
+
+  // Listen to "newRoomJoin" event
+  socket.on("newRoomJoin", ({ room, name }) => {
+    // If room and name is not provided redirect to home page
+    if((room === '' || room === undefined) && (name === '' || name === undefined)) {
+      io.to(socket.id).emit("joinError");
+    }
+
+    // Put new player into the room
+    socket.join(room);
+    const id = socket.id;
+    playerJoin(id, name, room);
+
+    // Get Room players
+    const playersInRoom = getRoomPlayers(room);
+
+    // If not enough players in room emit 'waiting' event
+    if(playersInRoom.length === 1){
+      io.to(room).emit("waiting");
+    }
+
+    if(playersInRoom.length === 2) {
+      io.to(room).emit("starting");
+    }
+  })
+
+  // removePlayer from rooms when disconnected
+  socket.on("disconnect", () => {
+    removePlayer(socket.id);
+    socket.disconnect();
+  });
+});
 
 // Serve static assets if in Production
 if(process.env.NODE_ENV === 'production') {
@@ -35,19 +92,9 @@ mongoose.connect(process.env.MONGODB_URI,
     useUnifiedTopology: true,
     useFindAndModify: false 
   }, (err) => {
-  if(err) return console.error(err);
+  if(err) return console.log(err);
 
   console.log("MongoDB Connection established.");
 });
 
-// Setup socket.io server
-const server = http.createServer(app);
-const io = socketio(server);
-
-
-// Run when a client connects
-io.on('connection', socket => {
-  console.log('A user connected');
-});
-
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}.`));
+server.listen(PORT, () => console.log(`Server started on port: ${PORT}.`));
