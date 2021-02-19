1b8a67be51d372627e2bb83f5fec4a1896dfe904
Jerome Mico Jaropojop <jmjaropojop92@gmail.com>
2021-02-19 16:29:00 +0800

    Installed 'socket.io' package and created a socket.io server in 'server.js';

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const mongoose = require('mongoose');
+const http = require('http');
+const socketio = require('socket.io');
 require('dotenv').config();
 
 // Set up Express
 const app = express();
 
 app.use(cors());
+
 app.use(express.json({ extended: false }));
+
+// Serve static assets if in Production
+if(process.env.NODE_ENV === 'production') {
+  // Set a static folder
+  app.use(express.static('build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 console.log("Starting server...");
 
-// Set up Monggose 
+// Connect to MongoDB 
 console.log("Connecting to MongoDB...")
 mongoose.connect(process.env.MONGODB_URI, 
   { 
@@ -24,6 +38,16 @@ mongoose.connect(process.env.MONGODB_URI,
   if(err) return console.error(err);
 
   console.log("MongoDB Connection established.");
+});
+
+// Setup socket.io server
+const server = http.createServer(app);
+const io = socketio(server);
+
+
+// Run when a client connects
+io.on('connection', socket => {
+  console.log('A user connected');
 });
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}.`));
